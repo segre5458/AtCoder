c788fb7757326d6c9b6f3baa3dbb887f38bcd3fd
segre <segrehouck5458@gmail.com>
2022-12-12 01:47:06 +0900

    add

---
+++ ABC281/a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <stack>
+#include <cstdlib>
+#include <queue>
+#include <set>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    for(int i=N; i>=0; i--){
+        cout << i << endl;
+    }
+    return 0;
+}
---
+++ ABC281/b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <stack>
+#include <cstdlib>
+#include <queue>
+#include <set>
+typedef long long ll;
+using namespace std;
+
+int main() {
+     
+    return 0;
+}
