77efd5cace3250e6b601288ca7c4240a44326abe
segre <segrehouck5458@gmail.com>
2022-12-12 01:57:25 +0900

    add

--- ABC281/b.cpp
+++ ABC281/b.cpp
@@ -1,17 +1,28 @@
-#include <iostream>
-#include <cstring>
-#include <string>
 #include <algorithm>
-#include <vector>
-#include <iomanip>
-#include <stack>
 #include <cstdlib>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
 #include <queue>
 #include <set>
+#include <stack>
+#include <string>
+#include <vector>
 typedef long long ll;
 using namespace std;
 
 int main() {
-     
-    return 0;
+  string s;
+  cin >> s;
+  bool flg = true;
+  if(s.size() != 8) flg = false;
+  if(!isupper(s[0])) flg = false;
+  for(int i=0; i<6; i++){
+    if(!isdigit(s[i+1])) flg = false;
+  }
+  if(s[1] - '0' == 0) flg = false;
+  if(!isupper(s[7])) flg = false;
+  if(flg) cout << "yes" << endl;
+  else cout << "no" << endl;
+  return 0;
 }
