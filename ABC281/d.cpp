55e273fd77be1c7448030b4ec64276e711e99b46
segre <segrehouck5458@gmail.com>
2022-12-13 12:59:45 +0900

    add

---
+++ ABC281/d.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <stack>
+#include <cstdlib>
+#include <queue>
+#include <set>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int N,K,D; cin >> N >> K >> D;
+    vector<int> a(N);
+    for(int i=0; i<N; i++) cin >> a[i];
+    
+    return 0;
+}
